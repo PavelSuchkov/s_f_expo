fe0ff99e798e15f44dab8b085f93814fe485dbc9
Pavel-Suchkou <suchkovp@gmail.com>
2022-02-22 22:59:18 +0300

    added bottom navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,24 +1,73 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import { MainScreen } from '../screens/MainScreen'
-import { PostScreen } from '../screens/PostScreen'
-import { THEME } from '../theme'
-import { Platform } from 'react-native'
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import {MainScreen} from '../screens/MainScreen'
+import {PostScreen} from '../screens/PostScreen'
+import {THEME} from '../theme'
+import {Platform} from 'react-native'
+import {BookedScreen} from '../screens/BookedScreen';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {Ionicons} from '@expo/vector-icons';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 
 
 const PostNavigator = createStackNavigator({
-  Main: MainScreen,
-  Post: {
-    screen: PostScreen,
-  },
-}, {
-  initialRouteName: 'Main',
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+    Main: MainScreen,
+    Post: {
+        screen: PostScreen,
     },
-    headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
-  },
+}, {
+    initialRouteName: 'Main',
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+        },
+        headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
 })
 
-export const AppNavigation = createAppContainer(PostNavigator)
+const BookedNavigator = createStackNavigator({
+    Booked: BookedScreen,
+    Post: PostScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+        },
+        headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
+})
+
+const bottomTabsConfig = {
+    Post: {
+        screen: PostNavigator,
+        navigationOptions: {
+            tabBarLabel: 'All',
+            tabBarIcon: (info) =>
+                <Ionicons name="ios-albums" size={25} color={info.tintColor}/>
+        }
+    },
+    Booked: {
+        screen: BookedNavigator,
+        navigationOptions: {
+            tabBarLabel: 'Booked',
+            tabBarIcon: (info) =>
+                <Ionicons name="ios-star" size={25} color={info.tintColor}/>
+        }
+    }
+}
+
+const BottomNavigator = Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+        activeTintColor: '#fff',
+        shifting: true,
+        barStyle: {
+            backgroundColor: THEME.MAIN_COLOR
+        }
+    })
+    : createBottomTabNavigator(bottomTabsConfig , {
+    tabBarOptions: {
+        activeTintColor: THEME.MAIN_COLOR
+    }
+})
+
+export const AppNavigation = createAppContainer(BottomNavigator)
--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,19 +1,44 @@
-import React from "react";
-import {Text, View } from "react-native";
+import React from 'react'
+import {Button, Text, View, FlatList} from 'react-native'
 import styled from 'styled-components'
+import {DATA} from '../data'
+import {Post} from '../components/Post'
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import {AppHeaderIcon} from '../components/AppHeaderIcon';
 
-export const BookedScreen = () => {
-  return (
-    <Container>
-      <Text>
-        BookedScreen
-      </Text>
-    </Container>
-  )
+export const BookedScreen = ({navigation}) => {
+
+    const openPostHandler = (post) => {
+        navigation.navigate('Post',
+            {postID: post.id, date: post.date, img: post.img, booked: post.booked})
+    }
+
+    return (
+        <Wrapper>
+            <FlatList data={DATA.filter(p => p.booked)}
+                      keyExtractor={post => post.id.toString()}
+                      renderItem={({item}) => <Post post={item}
+                                                    onOpen={openPostHandler}/>}
+            />
+
+
+        </Wrapper>
+    )
 }
 
-const Container = styled(View)`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+BookedScreen.navigationOptions = {
+    headerTitle: 'Booked',
+
+    headerLeft: <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item
+            title="Toggle drawer"
+            iconName="ios-menu"
+            onPress={() => console.log('pressed')}/>
+    </HeaderButtons>
+}
+
+
+const Wrapper = styled(View)`
+  padding: 3px;
 `
+
